eda8d24dc280272252be5a494f8c86189049ac60
Lucas Simão <novellysimaolucas@gmail.com>
2025-07-30 20:18:23 -0300

    Fix typo in input prompt and ensure correct output display for addition exercise

--- exercicio2.java
+++ exercicio2.java
@@ -10,10 +10,21 @@ public class exercicio2 {
         int soma1;
         int soma2;
 
-        System.out.println("Digite op primeiro valor da soma: ");
+        System.out.println("Digite o primeiro valor da soma: ");
         soma1 = kb.nextInt();
         System.out.println("Digite o segundo valor da soma:  ");
         soma2 = kb.nextInt();
+
+        int resultado;
         
+        for(int i = soma2; i>-1; i--){
+            resultado = soma1 + soma2;
+            System.out.println("Número: " + soma1);
+            System.out.println("Número: " + soma2);
+            System.out.println("Resultado:" + resultado);
+
+            soma1++;
+            soma2--;
+        }
     }
 }
